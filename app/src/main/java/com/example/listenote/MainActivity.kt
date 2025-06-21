44ea06da9eefa5ff46138c7d6c57ccd4e5aabcf4
working-y-mizutani <217286516+working-y-mizutani@users.noreply.github.com>
2025-06-21 11:40:32 +0900

    feat: トップ画面作成

--- app/src/main/java/com/example/listenote/MainActivity.kt
+++ app/src/main/java/com/example/listenote/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.listenote.ui.theme.ListenoteTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +27,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ListenoteTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    TopView(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +37,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TopView(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(
+            space = 200.dp,
+            alignment = Alignment.CenterVertically
+        ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = { /* TODO: メモ作成の処理 */ },
+            modifier = Modifier
+                .fillMaxWidth(0.6f)
+                .height(100.dp)
+        ) {
+            Text(text = "メモ作成")
+        }
+
+        Button(
+            onClick = { /* TODO: メモ一覧の処理 */ },
+            modifier = Modifier
+                .fillMaxWidth(0.6f)
+                .height(100.dp)
+        ) {
+            Text(text = "メモ一覧")
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ListenoteTheme {
-        Greeting("Android")
+        TopView()
     }
 }
