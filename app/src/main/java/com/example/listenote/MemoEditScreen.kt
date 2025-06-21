1874cb030f997ed210f0abd729456697ff9af910
working-y-mizutani <217286516+working-y-mizutani@users.noreply.github.com>
2025-06-21 23:15:24 +0900

    feat: topBarに戻る、保存、削除アイコンを設置
    処理の実装はまだ

--- app/src/main/java/com/example/listenote/MemoEditScreen.kt
+++ app/src/main/java/com/example/listenote/MemoEditScreen.kt
@@ -9,10 +9,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -88,89 +97,105 @@ fun MemoEditScreen(modifier: Modifier = Modifier) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MemoDetailScreen(modifier: Modifier = Modifier) {
 
     var impression by remember { mutableStateOf("") }
     var toDo by remember { mutableStateOf("") }
 
-    Column(
-        modifier = modifier.fillMaxSize()
-    ) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("メモの詳細") },
+                navigationIcon = {
+                    IconButton(onClick = { /* TODO: 戻る処理 */ }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "戻る"
+                        )
+                    }
+                },
+                actions = {
+                    // 保存ボタン
+                    IconButton(onClick = { /* TODO: 保存処理 */ }) {
+                        Icon(
+                            imageVector = Icons.Default.Check,
+                            contentDescription = "保存"
+                        )
+                    }
+                    // 削除ボタン
+                    IconButton(onClick = { /* TODO: 削除処理 */ }) {
+                        Icon(
+                            imageVector = Icons.Default.Delete,
+                            contentDescription = "削除"
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
 
         Column(
-            modifier = Modifier
-                .weight(0.7f)
-                .padding(24.dp)
-
+            modifier = modifier
+                .fillMaxSize()
+                .padding(innerPadding)
         ) {
 
-            val labelWidth = 72.dp
-            val spaceBetween = 12.dp
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = "時間",
-                    modifier = Modifier.width(labelWidth),
-                )
-                Text(text = "00:00")
-            }
-
-            Spacer(modifier = Modifier.height(spaceBetween))
-
-            Row(
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f),
-                verticalAlignment = Alignment.Top
-            ) {
-                Text(
-                    text = "感想",
-                    modifier = Modifier.width(labelWidth),
-                )
-                TextField(
-                    value = impression,
-                    onValueChange = { newText ->
-                        impression = newText
-                    },
-                    modifier = Modifier.fillMaxSize(),
+                    .weight(0.7f)
+                    .padding(horizontal = 8.dp)
+                    .padding(bottom = 16.dp)
 
+            ) {
+                val labelWidth = 68.dp
+                val spaceBetween = 12.dp
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = "時間", modifier = Modifier.width(labelWidth))
+                    Text(text = "00:00")
+                }
+                Spacer(modifier = Modifier.height(spaceBetween))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    verticalAlignment = Alignment.Top
+                ) {
+                    Text(text = "感想", modifier = Modifier.width(labelWidth))
+                    TextField(
+                        value = impression,
+                        onValueChange = { impression = it },
+                        modifier = Modifier.fillMaxSize()
                     )
+                }
+                Spacer(modifier = Modifier.height(spaceBetween))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    verticalAlignment = Alignment.Top
+                ) {
+                    Text(text = "ToDo", modifier = Modifier.width(labelWidth))
+                    TextField(
+                        value = toDo,
+                        onValueChange = { toDo = it },
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
             }
 
-            Spacer(modifier = Modifier.height(spaceBetween))
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f),
-                verticalAlignment = Alignment.Top
-            ) {
-                Text(
-                    text = "ToDo",
-                    modifier = Modifier.width(labelWidth),
-                )
-                TextField(
-                    value = toDo,
-                    onValueChange = { newText ->
-                        toDo = newText
-                    },
-                    modifier = Modifier.fillMaxSize(),
-                )
-            }
-
-
+            PlayerUI(
+                audioResourceId = R.raw.test_my_song,
+                Modifier
+                    .weight(0.3f)
+                    .background(Color.Green)
+            )
         }
-
-        PlayerUI(
-            audioResourceId = R.raw.test_my_song,
-            Modifier
-                .weight(0.3f)
-                .background(Color.Green)
-        )
     }
 }
 
