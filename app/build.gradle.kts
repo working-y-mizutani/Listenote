f2770d6ec4f0f65fc4fe1d8da696b870a0cfdc73
working-y-mizutani <217286516+working-y-mizutani@users.noreply.github.com>
2025-06-22 10:54:36 +0900

    chore: kspとroomの依存関係を追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")version "2.1.21-2.0.2"
 }
 
 android {
@@ -59,5 +60,9 @@ dependencies {
     implementation(libs.androidx.media3.exoplayer)
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
 
 }
