3405f4ec6c8383d93199d5de8bf1d9b08c12cda0
working-y-mizutani <217286516+working-y-mizutani@users.noreply.github.com>
2025-06-26 23:12:53 +0900

    refactor:MainActivity内の処理を関数に切り分けた
    onCreateが大きくなってきたのでUIの記述は切り分けた。

--- app/src/main/java/com/example/listenote/MainActivity.kt
+++ app/src/main/java/com/example/listenote/MainActivity.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -49,85 +50,92 @@ class MainActivity : ComponentActivity() {
             ListenoteTheme {
                 val navController = rememberNavController()
                 val audioPlayerViewModel: AudioPlayerViewModel = viewModel()
-
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                    NavHost(
-                        navController = navController,
-                        startDestination = "top", // 開始画面
-                        modifier = Modifier.padding(innerPadding)
-                    ) {
-                        // Top画面のルート
-                        // composableでNavHostに情報を登録
-                        // navController.navigate(routeName)で {} 内の処理を実行
-                        composable("top") {
-                            TopView(navController = navController)
-                        }
-                        // メモ編集画面のルート
-                        composable(
-                            "notebook/{notebookId}",
-                            arguments = listOf(navArgument("notebookId") {
-                                type = NavType.LongType
-                            })
-                            // navController.navigate()が呼ばれると、この画面の情報がbackStackに置かれる。
-                            // そして、このcomposableブロックが実行される際に、
-                            // 引数の backStackEntry から arguments を取り出すことができる。
-                        ) { backStackEntry ->
-                            val notebookId = backStackEntry.arguments?.getLong("notebookId")
-                            if (notebookId != null) {
-                                NotebookScreen(
-                                    viewModel = viewModel(
-                                        factory = NotebookViewModelFactory(
-                                            application = LocalContext.current.applicationContext as Application,
-                                            notebookId = notebookId
-                                        )
-                                    ),
-                                    navController = navController,
-                                    audioPlayerViewModel = audioPlayerViewModel
-                                )
-                            }
-                        }
-                        composable(
-                            // メモ新規作成の場合 memoIdはないためオプションにする
-                            // 既存メモ編集の場合memoIdが渡される
-                            route = "memo_create_edit/{notebookId}?memoId={memoId}&timestamp={timestamp}",
-                            arguments = listOf(
-                                navArgument("notebookId") { type = NavType.LongType },
-                                navArgument("memoId") {
-                                    type = NavType.LongType
-                                    defaultValue = -1L
-                                },
-                                navArgument("timestamp") {
-                                    type = NavType.LongType
-                                    defaultValue = 0L
-                                },
-                            )
-                        ) { backStackEntry ->
-                            val notebookId = backStackEntry.arguments?.getLong("notebookId")
-                            val memoId = backStackEntry.arguments?.getLong("memoId")
-                            val timestamp = backStackEntry.arguments?.getLong("timestamp")
-                            if (notebookId != null && memoId != null && timestamp != null) {
-                                MemoCreateEditScreen(
-                                    navController = navController,
-                                    audioPlayerViewModel = audioPlayerViewModel,
-                                    viewModel = viewModel(
-                                        factory = MemoCreateEditViewModelFactory(
-                                            application = LocalContext.current.applicationContext as Application,
-                                            notebookId = notebookId,
-                                            memoId = memoId,
-                                            timestamp = timestamp,
-                                        )
-                                    )
-                                )
-                            }
-
-                        }
-                    }
-                }
+                ListenoteApp(navController, audioPlayerViewModel)
             }
         }
     }
 }
+
+@Composable
+fun ListenoteApp(
+    navController: NavHostController,
+    audioPlayerViewModel: AudioPlayerViewModel
+) {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = "top", // 開始画面
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            // Top画面のルート
+            // composableでNavHostに情報を登録
+            // navController.navigate(routeName)で {} 内の処理を実行
+            composable("top") {
+                TopView(navController = navController)
+            }
+            // メモ編集画面のルート
+            composable(
+                "notebook/{notebookId}",
+                arguments = listOf(navArgument("notebookId") {
+                    type = NavType.LongType
+                })
+                // navController.navigate()が呼ばれると、この画面の情報がbackStackに置かれる。
+                // そして、このcomposableブロックが実行される際に、
+                // 引数の backStackEntry から arguments を取り出すことができる。
+            ) { backStackEntry ->
+                val notebookId = backStackEntry.arguments?.getLong("notebookId")
+                if (notebookId != null) {
+                    NotebookScreen(
+                        viewModel = viewModel(
+                            factory = NotebookViewModelFactory(
+                                application = LocalContext.current.applicationContext as Application,
+                                notebookId = notebookId
+                            )
+                        ),
+                        navController = navController,
+                        audioPlayerViewModel = audioPlayerViewModel
+                    )
+                }
+            }
+            composable(
+                // メモ新規作成の場合 memoIdはないためオプションにする
+                // 既存メモ編集の場合memoIdが渡される
+                route = "memo_create_edit/{notebookId}?memoId={memoId}&timestamp={timestamp}",
+                arguments = listOf(
+                    navArgument("notebookId") { type = NavType.LongType },
+                    navArgument("memoId") {
+                        type = NavType.LongType
+                        defaultValue = -1L
+                    },
+                    navArgument("timestamp") {
+                        type = NavType.LongType
+                        defaultValue = 0L
+                    },
+                )
+            ) { backStackEntry ->
+                val notebookId = backStackEntry.arguments?.getLong("notebookId")
+                val memoId = backStackEntry.arguments?.getLong("memoId")
+                val timestamp = backStackEntry.arguments?.getLong("timestamp")
+                if (notebookId != null && memoId != null && timestamp != null) {
+                    MemoCreateEditScreen(
+                        navController = navController,
+                        audioPlayerViewModel = audioPlayerViewModel,
+                        viewModel = viewModel(
+                            factory = MemoCreateEditViewModelFactory(
+                                application = LocalContext.current.applicationContext as Application,
+                                notebookId = notebookId,
+                                memoId = memoId,
+                                timestamp = timestamp,
+                            )
+                        )
+                    )
+                }
+
+            }
+        }
+    }
+}
+
 
 @Composable
 fun TopView(navController: NavController, modifier: Modifier = Modifier) {
