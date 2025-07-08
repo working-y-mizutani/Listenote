5c9d69a3a7b69a29be810048558dd04e6febfc1b
pc14 <pc14@PC14>
2025-07-08 13:09:35 +0900

    feat:フォント関係のファイルを追加、設定を編集した

--- app/src/main/java/com/example/listenote/ui/theme/Type.kt
+++ app/src/main/java/com/example/listenote/ui/theme/Type.kt
@@ -1,34 +1,153 @@
 package com.example.listenote.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.listenote.R
+import androidx.compose.ui.text.font.Font
+
+// Variable Fontを使用してFontFamilyを定義
+@OptIn(ExperimentalTextApi::class)
+val NotoSansJpFamily = FontFamily(
+    Font(
+        resId = R.font.noto_sansjp_variable_font_wght,
+        weight = FontWeight.Light,
+        // variationSettingsで対応する'wght'軸の値を指定
+        variationSettings = FontVariation.Settings(FontVariation.weight(300))
+    ),
+    Font(
+        resId = R.font.noto_sansjp_variable_font_wght,
+        weight = FontWeight.Normal,
+        variationSettings = FontVariation.Settings(FontVariation.weight(400))
+    ),
+    Font(
+        resId = R.font.noto_sansjp_variable_font_wght,
+        weight = FontWeight.Medium,
+        variationSettings = FontVariation.Settings(FontVariation.weight(500))
+    ),
+    Font(
+        resId = R.font.noto_sansjp_variable_font_wght,
+        weight = FontWeight.Bold,
+        variationSettings = FontVariation.Settings(FontVariation.weight(700))
+    ),
+    Font(
+        resId = R.font.noto_sansjp_variable_font_wght,
+        weight = FontWeight.Black,
+        variationSettings = FontVariation.Settings(FontVariation.weight(950))
+    )
+)
+
+val ZenKakuGothicFamily = FontFamily(
+    Font(R.font.zen_kaku_gothic_new_regular, FontWeight.Normal),
+    Font(R.font.zen_kaku_gothic_new_medium, FontWeight.Medium),
+    Font(R.font.zen_kaku_gothic_new_bold, FontWeight.Bold),
+    Font(R.font.zen_kaku_gothic_new_light, FontWeight.Light),
+    Font(R.font.zen_kaku_gothic_new_black, FontWeight.Black),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    headlineLarge = TextStyle(
+        fontFamily = ZenKakuGothicFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = ZenKakuGothicFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = NotoSansJpFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = ZenKakuGothicFamily,
+        fontWeight = FontWeight.Medium,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+    titleMedium = TextStyle(
+        fontFamily = ZenKakuGothicFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = ZenKakuGothicFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+    ),
+
+    bodyLarge = TextStyle(
+        fontFamily = NotoSansJpFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = NotoSansJpFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = NotoSansJpFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+    ),
+
+    labelLarge = TextStyle(
+        fontFamily = NotoSansJpFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = NotoSansJpFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+    ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NotoSansJpFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
+    ),
+
+    displayLarge = TextStyle(
+        fontFamily = NotoSansJpFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 57.sp,
+        lineHeight = 64.sp,
+    ),
+    displayMedium = TextStyle(
+        fontFamily = NotoSansJpFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
+    ),
+    displaySmall = TextStyle(
+        fontFamily = NotoSansJpFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+    ),
+
     )
-    */
-)
